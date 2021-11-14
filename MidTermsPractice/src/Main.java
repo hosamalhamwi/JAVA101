ae7e31989811aa0a15887dde5d28e2ec9a4f1f92
Muhammad Qazi <muhammedqazi@arsinex.com>
2021-11-14 09:30:15 +0200

    Update Main.java

--- MidTermsPractice/src/Main.java
+++ MidTermsPractice/src/Main.java
@@ -5,77 +5,80 @@ import java.text.SimpleDateFormat;
 public class Main {
     public static void main(String[] args) throws ParseException {
 
-//        Silly silly = new Silly("John");
-//        silly.showMessage();
-//
-//
-//        String name = JOptionPane.showInputDialog(null , "Please enter the name :");
-//        String surname = JOptionPane.showInputDialog(null , "Please enter the surname :");
-//
-//        Reverse reverse = new Reverse(name , surname);
-//        reverse.showReverse();
-
-//        Person john = new Person(21 , "John");
-//        JOptionPane.showMessageDialog(null , "Age before calling Birthday() :" + john.getAge());
-//        john.birthday();
-//        JOptionPane.showMessageDialog(null , "Age after calling Birthday() :" + john.getAge());
-//
-//
-//
-//        Person susan = new Person();
-//        susan.setAge(25);
-//        susan.setName("Susan");
-//        JOptionPane.showMessageDialog(null , "Susan :" + susan.getAge() + " " + susan.getName());
+        Silly silly = new Silly("John");
+        silly.showMessage();
 
 
-//        Engineer jack = new Engineer("Jack" , 32);
-//        jack.setJob("Computer Engineer");
-//        jack.setSalary(2250);
-//        jack.addBonusToSalary(1000);
+        String name = JOptionPane.showInputDialog(null , "Please enter the name :");
+        String surname = JOptionPane.showInputDialog(null , "Please enter the surname :");
 
-//        String Data  = jack.getName() + " " + jack.getAge() + " " + jack.getJob() + " " + jack.getSalary();
-//        JOptionPane.showMessageDialog(null, Data);
+        Reverse reverse = new Reverse(name , surname);
+        reverse.showReverse();
 
-//        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-//
-//        Employee e1 = new Employee("John" , "man" ,"Engineer" );
-//        e1.setDOB(formatter.parse("12-12-1989"));
-//        e1.setFullTime(true);
-//        e1.setFullTimeSalary(2500);
-//        String e1Data = e1.getEmployeeName() + " " +
-//                e1.getEmployeeSex() + " " + e1.getEmployeePosition() + " " + e1.getEmployeeDOB() + " " +
-//                e1.getSalary() + " " + e1.getFullTime();
-//
-//        JOptionPane.showMessageDialog(null , e1Data);
-//
-//
-//        Employee e2 = new Employee("Jane" , "Woman" , "consultant");
-//        e2.setDOB(formatter.parse("12-12-1989"));
-//        e2.setFullTime(false);
-//        e2.setPartTimeHours(120);
-//        e2.getSalary();
-//        e2.setDOB(formatter.parse("12-12-1989"));
-//
-//        String e2Data = e2.getEmployeeName() + " " +
-//                e2.getEmployeeSex() + " " + e2.getEmployeePosition() + " " + e2.getEmployeeDOB() + " " +
-//                e2.getSalary() + " " + e2.getFullTime();
-//        JOptionPane.showMessageDialog(null , e2Data);
+        Person john = new Person(21 , "John");
+        JOptionPane.showMessageDialog(null , "Age before calling Birthday() :" + john.getAge());
+        john.birthday();
+        JOptionPane.showMessageDialog(null , "Age after calling Birthday() :" + john.getAge());
 
-//        GUI gui = new GUI("Authentication");
-//        gui.setVisible(true);
 
-//        BtnForm form = new BtnForm("Colors Button Form");
-//        form.setVisible(true);
 
-//        AnimalForm animal = new AnimalForm("Animal Form");
-//        animal.setVisible(true);
+        Person susan = new Person();
+        susan.setAge(25);
+        susan.setName("Susan");
+        JOptionPane.showMessageDialog(null , "Susan :" + susan.getAge() + " " + susan.getName());
 
-//        CheckRadioForm check = new CheckRadioForm();
-//        check.setVisible(true);
+
+        Engineer jack = new Engineer("Jack" , 32);
+        jack.setJob("Computer Engineer");
+        jack.setSalary(2250);
+        jack.addBonusToSalary(1000);
+
+        String Data  = jack.getName() + " " + jack.getAge() + " " + jack.getJob() + " " + jack.getSalary();
+        JOptionPane.showMessageDialog(null, Data);
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+
+        Employee e1 = new Employee("John" , "man" ,"Engineer" );
+        e1.setDOB(formatter.parse("12-12-1989"));
+        e1.setFullTime(true);
+        e1.setFullTimeSalary(2500);
+        String e1Data = e1.getEmployeeName() + " " +
+                e1.getEmployeeSex() + " " + e1.getEmployeePosition() + " " + e1.getEmployeeDOB() + " " +
+                e1.getSalary() + " " + e1.getFullTime();
+
+        JOptionPane.showMessageDialog(null , e1Data);
+
+
+        Employee e2 = new Employee("Jane" , "Woman" , "consultant");
+        e2.setDOB(formatter.parse("12-12-1989"));
+        e2.setFullTime(false);
+        e2.setPartTimeHours(120);
+        e2.getSalary();
+        e2.setDOB(formatter.parse("12-12-1989"));
+
+        String e2Data = e2.getEmployeeName() + " " +
+                e2.getEmployeeSex() + " " + e2.getEmployeePosition() + " " + e2.getEmployeeDOB() + " " +
+                e2.getSalary() + " " + e2.getFullTime();
+        JOptionPane.showMessageDialog(null , e2Data);
+
+        GUI gui = new GUI("Authentication");
+        gui.setVisible(true);
+
+        BtnForm form = new BtnForm("Colors Button Form");
+        form.setVisible(true);
+
+        AnimalForm animal = new AnimalForm("Animal Form");
+        animal.setVisible(true);
+
+        CheckRadioForm check = new CheckRadioForm();
+        check.setVisible(true);
 
 
         ComboBox combo = new ComboBox();
         combo.setVisible(true);
+
+        Menu menu = new Menu();
+        menu.setVisible(true);
 
 
 
