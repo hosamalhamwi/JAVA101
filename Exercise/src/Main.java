1b819009fb9cfa5f9531bc7ad96b83236bec391e
Muhammad Qazi <muhammedqazi@arsinex.com>
2021-11-16 16:21:18 +0200

    Update Main.java

--- Exercise/src/Main.java
+++ Exercise/src/Main.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
+import java.text.SimpleDateFormat;
 
-public class Main {
+public class Test {
    public static void main(String[] args){
        Person p1 = new Person(12 , "John");
        p1.birthDay();
@@ -20,11 +21,32 @@ public class Main {
 
        JOptionPane.showMessageDialog(null,jack.getName() + " " + jack.getAge() + " " + jack.getJobTitle() + " " + jack.getSalary());
 
-       Employee em = new Employee("John" , "man" , "Engineer");
-       em.setFullTimeSalary(2500);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+
+        Employee e1 = new Employee("John" , "man" ,"Engineer" );
+        e1.setDOB(formatter.parse("12-12-1989"));
+        e1.setFullTime(true);
+        e1.setFullTimeSalary(2500);
+        String e1Data = e1.getEmployeeName() + " " +
+                e1.getEmployeeSex() + " " + e1.getEmployeePosition() + " " + e1.getEmployeeDOB() + " " +
+                e1.getSalary() + " " + e1.getFullTime();
+
+        JOptionPane.showMessageDialog(null , e1Data);
 
 
-       JOptionPane.showMessageDialog(null ,em.getEmployeeName() + " " + em.getEmployeeSex() + " " + em.getEmployeePosition() + " " + em.getSalary());
+        Employee e2 = new Employee("Jane" , "Woman" , "consultant");
+        e2.setDOB(formatter.parse("12-12-1989"));
+        e2.setFullTime(false);
+        e2.setPartTimeHours(120);
+        e2.getSalary();
+        e2.setDOB(formatter.parse("12-12-1989"));
+
+        String e2Data = e2.getEmployeeName() + " " +
+                e2.getEmployeeSex() + " " + e2.getEmployeePosition() + " " + e2.getEmployeeDOB() + " " +
+                e2.getSalary() + " " + e2.getFullTime();
+        JOptionPane.showMessageDialog(null , e2Data);
+
+
 
 
 
