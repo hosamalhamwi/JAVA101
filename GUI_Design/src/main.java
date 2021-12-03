caeaaca37aaca8a489b6cf0254df5eedbf489803
Muhammad Qazi <muhammedqazi@arsinex.com>
2021-12-03 13:18:01 +0300

    Delete GUI_Design directory

--- GUI_Design/src/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        Form f = new Form("Form");
-        f.setVisible(true);
-
-        Picture p = new Picture("Picture");
-        p.setVisible(true);
-    }
-}
