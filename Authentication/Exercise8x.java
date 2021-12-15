88f0f685b97a028181ed0a054a64aa5618fed442
Muhammad Qazi <tigerbhai888@gmail.com>
2021-12-15 20:03:28 +0200

    Authentications

---
+++ Authentication/Exercise8x.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise8x;
+
+/**
+ *
+ * @author muhammad
+ * NetBeans
+ */
+public class Exercise8x {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Panel panel = new Panel();
+        
+        panel.setVisible(true);
+        
+    }
+    
+}
